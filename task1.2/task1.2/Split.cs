3dcebafde6a2e1e84dff4dc9971b1214078482e3
Mehrac Mammadov <mehrajvm@code.edu.az>
2022-11-27 20:28:05 +0400

    all tasks done

---
+++ task1.2/task1.2/Split.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1._2
+{
+    internal static class Split
+    {
+        public static string[] CustomSplit(this string sentence, char symbol = ' ')
+        {
+            string str = "";
+            string[] arr = new string[0];
+
+            for (int i=0; i<sentence.Length; i++)
+            {
+                if (sentence[i] == symbol)
+                {
+                    Array.Resize(ref arr, arr.Length+1);
+                    arr[arr.Length - 1] = str;
+                    str = string.Empty;
+                }
+                else
+                {
+                    str+= sentence[i];
+                }
+            }
+
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = str;
+
+            return arr;
+        }
+            
+    }
+}
